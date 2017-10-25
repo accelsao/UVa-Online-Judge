257903b7c6725a41448f453f877740556f8b88d7
accelsao <jay70550@yahoo.com.tw>
2017-10-25 12:48:55 +0800

    Update 1169 - Robotruck.cpp

--- Volume 011/1169 - Robotruck.cpp
+++ Volume 011/1169 - Robotruck.cpp
@@ -1,47 +1,54 @@
 #include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<cstring>
+#include<cstdio>
+#include<queue>
 #include<string>
-#include<string>
-#include<sstream>
-#include<fstream>
 #include<map>
 #include<set>
+#include<string>
+#include<sstream>
 #include<vector>
-#include<algorithm>
-#include<deque>
+#include<functional>
+#include<fstream>
+#include<stack>
+#include<vector>
 using namespace std;
 
-int n, m, k;
-const int M = 100005;
-int dp[M], cost[M], d[M], x[M], y[M], w[M];
-deque<int>q;
+
+//dp[i] = (dp[j - 1] - cost[j] + dist[j]) + cost[i] + dist[i]
+int cost[100001], d[100001];
+int p[100001][3];
+int dp[100001];
 int f(int j) {
 	return dp[j - 1] - cost[j] + d[j];
 }
 int main() {
 	int t;
 	cin >> t;
-	int c, n;
-	while (t--){
+	while (t--) {
+		int c, n;
 		cin >> c >> n;
-		w[0] = cost[0] = x[0] = y[0] = 0;
-		for (int i = 1; i <= n; i++){
-			cin >> x[i] >> y[i] >> w[i];
-			cost[i] = cost[i - 1] + abs(x[i] - x[i - 1]) + abs(y[i] - y[i - 1]);
-			w[i] += w[i - 1];
-			d[i] = abs(x[i]) + abs(y[i]);
+		p[0][0] = p[0][1] = p[0][2] = cost[0] = 0;
+		for (int i = 1; i <= n; i++) {
+			cin >> p[i][0] >> p[i][1] >> p[i][2];
+			cost[i] = cost[i - 1] + abs(p[i][0] - p[i - 1][0]) + abs(p[i][1] - p[i - 1][1]);
+			p[i][2] += p[i - 1][2];
+			d[i] = abs(p[i][0]) + abs(p[i][1]);
 		}
-		q.clear();
-		q.push_back(0);
-		for (int i = 1; i <= n; i++){
-			while (!q.empty() && w[i] - w[q.front()] > c)
-				q.pop_front();
-			dp[i] = f(q.front() + 1) + cost[i] + d[i];
-			while (!q.empty() && f(q.back() + 1) >= f(i + 1))
+		deque<int>q;
+		for (int i = 1; i <= n; i++) {
+			while (!q.empty() && f(i) <= f(q.back()))
 				q.pop_back();
 			q.push_back(i);
+			while (!q.empty() && p[i][2] - p[q.front() - 1][2] > c)
+				q.pop_front();
+			dp[i] = f(q.front())+ cost[i] + d[i];
 		}
 		printf("%d\n", dp[n]);
-		if (t)puts("");
+		if (t)
+			puts("");
 	}
 }
 // dp[i] = dp[j-1] + cost[j, i] + dist[j] + dist[i]  j<=i
