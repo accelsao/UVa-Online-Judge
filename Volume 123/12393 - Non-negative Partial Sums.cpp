19ea0363ef67afd4216e8c5942af6f33b7b4a265
accelsao <jay70550@yahoo.com.tw>
2017-10-26 22:07:16 +0800

    Update 12393 - Non-negative Partial Sums.cpp

--- Volume 123/12393 - Non-negative Partial Sums.cpp
+++ Volume 123/12393 - Non-negative Partial Sums.cpp
@@ -17,7 +17,9 @@
 #include <ctime>
 using namespace std;
 
-
+/* n為長度  
+S[i, i+n-1] 的最小值 要大於等於 S[i]
+*/
 
 
 const int N = 2000005;
@@ -32,11 +34,11 @@ int main() {
 		deque<int>q;
 		int ans = 0;
 		for (int i = 0; i < n * 2; i++) {
-			while (!q.empty() && s[q.back()] >= s[i])
+			while (!q.empty() && s[q.back()] >= s[i])//放入區間最小 維護小到大
 				q.pop_back();
 			q.push_back(i);
-			if (i >= n&&s[q.front()] - s[i - n] >= 0)ans++;
-			while (!q.empty() && i - q.front() + 1 > n)q.pop_front();
+			if (i >= n&&s[q.front()] - s[i - n] >= 0)ans++;//區間最小 >=s[i] 合法
+			while (!q.empty() && i - q.front() + 1 > n)q.pop_front();//超過長度 
 		}
 		printf("%d\n", ans);
 	}
