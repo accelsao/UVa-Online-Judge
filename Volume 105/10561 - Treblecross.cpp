353b1811d83e19d1eb0d57a808a36543defbcf7a
accelsao <jay70550@yahoo.com.tw>
2017-11-02 16:30:24 +0800

    Update 10561 - Treblecross.cpp

--- Volume 105/10561 - Treblecross.cpp
+++ Volume 105/10561 - Treblecross.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 const int maxn = 205;
-
 int g[maxn], v[maxn];
 
 int SG(int s) {
@@ -57,23 +56,22 @@ bool check() {
 bool judge() {
 	n = 0;
 	len = str.size();
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < len; i++) 
 		if (str[i] == '.') {
 			str[i] = 'X';
 			if (check())
 				pos[n++] = i + 1;
+			else if (i && i < len - 1 & str[i - 1] == 'X' && str[i + 1] == 'X')
+				pos[n++] = i + 1;
+
+			else if (i < len - 2 && str[i + 1] == 'X' && str[i + 2] == 'X')
+				pos[n++] = i + 1;
+
+			else if (i >= 2 && str[i - 1] == 'X' && str[i - 2] == 'X')
+				pos[n++] = i + 1;
 			str[i] = '.';
 		}
-
-		if (i && i < len - 1 & str[i - 1] == 'X' && str[i + 1] == 'X')
-			pos[n++] = i + 1;
-
-		if (i < len - 2 && str[i + 1] == 'X' && str[i + 2] == 'X')
-			pos[n++] = i + 1;
-
-		if (i >= 2 && str[i - 1] == 'X' && str[i - 2] == 'X')
-			pos[n++] = i + 1;
-	}
+	
 	return n;
 }
 
