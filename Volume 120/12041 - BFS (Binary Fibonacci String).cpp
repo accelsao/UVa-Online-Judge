88fc4c71b80126997b12ed015a841f8f73e4837d
accelsao <jay70550@yahoo.com.tw>
2019-03-01 11:26:23 +0800

    add 12041

---
+++ Volume 120/12041 - BFS (Binary Fibonacci String).cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#define LL long long
+#define Accel ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define Re(a,b) memset(a,b,sizeof a)
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, A, B) for (int I = (A); I <= (B); I++)
+#define REP(I, N) for (int I = 0; I < (N); I++)
+#define PII pair<int,int>
+#define PLL pair<LL,LL>
+#define SZ(a) ((int)a.size())
+//#pragma GCC optimize(2)
+using namespace std;
+const int N=1e5+1;
+const int M=998244353;
+const double eps=1e-6;
+typedef complex<double>C;
+const double PI(acos(-1.0));
+//for(int mask=i;mask>0;mask=(mask-1)&i)
+//int a[25]= { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+//__gcd, atan2(y,x)=y/x , __int128
+//c(n,k)=c(n-1,k-1)+c(n-1,k)
+//c(i,r),i for r to n =c(n+1,r+1)
+//void add(int x){while(x<N)BIT[x]++,x+=x&-x;}
+//void add(int x){while(x<N)BIT[x]++,x+=x&-x;}
+//int sum(int x){int t=0;while(x){t+=BIT[x];x-=x&-x;}return t;}
+//LL pw(LL a,LL b){LL t=1;for(;b;b>>=1,a=a*a%M)b&1?t=t*a%M:0;return t;}
+//log() = ln() , log(x)/log(y)=log(y)-base-x
+//INT_MAX 127,INT_MIN 128
+//int dw[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+//int dw[8][2]={{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+//cout<<fixed<<setprecision(12)<<ans<<endl;
+//__builtin_popcount(mask)
+
+
+
+
+int t;
+LL f[N];
+void dfs(int n,int i,int j){
+	if(n==0){
+		cout<<0;return ;
+	}
+	if(n==1){
+		cout<<1;return ;
+	}
+	if(n==2){
+		if(i==1&&j==1)cout<<0;
+		else if(i==1&&j==2)cout<<"01";
+		else cout<<1;
+		return ;
+	}
+	if(i>f[n-2]){
+		dfs(n-1,i-f[n-2],j-f[n-2]);
+		return ;
+	}
+	if(j<=f[n-2]){
+		dfs(n-2,i,j);
+		return ;	
+	}
+	dfs(n-2,i,f[n-2]);
+	dfs(n-1,1,j-f[n-2]);
+	
+}
+int main(){
+	
+	//freopen("01.txt","w",stdout);
+	
+	f[0]=1;
+	f[1]=1;
+	for(int i=2;i<=47;i++){
+		f[i]=f[i-1]+f[i-2];
+//		cout<<f[i]<<endl;
+	}
+
+	int t;
+	cin>>t;
+	int n,i,j;
+	while(t--){
+		cin>>n>>i>>j;
+		if(n%2==0)
+			n=min(n,46);
+		else
+			n=min(n,47);
+		dfs(n,i+1,j+1);
+		cout<<endl;
+	}
+	
+	
+}
+/*
+3
+6
+1 2 1 3 1 2
+4
+2 4 2 4
+9
+10 2 2 10 3 4 5 4 3
+*/
